f2a00867fbb208ef2653f453a9c1f29f976c0c04
Matheus <matheusaroxa13@gmail.com>
2026-04-30 18:57:15 -0300

    Navbar

--- frontend/src/app/pages/products/products.ts
+++ frontend/src/app/pages/products/products.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ Navbar ],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
