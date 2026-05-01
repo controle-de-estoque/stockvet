0cf43f3ebb587b12db8b20da684bd617760ed24f
Matheus <matheusaroxa13@gmail.com>
2026-05-01 10:54:37 -0300

    Feat: Responsividade na navbar

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  isMenuOpen = false;
 
   constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
   logout() {
     localStorage.removeItem('token');
