f5321f79e5cfd56a141ebaa9f6e5b8b1af62ac4a
matheut <matheus.teixeira@tjpe.jus.br>
2026-05-12 10:11:16 -0300

    Feat: Pagina admin primitiva

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,9 @@ import { CadastrarUnidade } from './pages/cadastrar-unidade/cadastrar-unidade';
 import { CadastrarCategoria } from './pages/cadastrar-categoria/cadastrar-categoria';
 import { Movimentacoes } from './pages/movimentacoes/movimentacoes';
 import { CadastrarMovimentacao } from './pages/cadastrar-movimentacao/cadastrar-movimentacao';
+import { Admin } from './pages/admin/admin';
+import { CadastrarUsuario } from './pages/cadastrar-usuario/cadastrar-usuario';
+import { CadastrarCessionario } from './pages/cadastrar-cessionario/cadastrar-cessionario';
 
 export const routes: Routes = [
   {
@@ -35,8 +38,20 @@ export const routes: Routes = [
     component: Movimentacoes
   },
   {
+    path: 'admin',
+    component: Admin
+  },
+  {
     path: 'cadastrar-produtos',
     component: CadastrarProduto
+  },
+  {
+    path: 'cadastrar-usuario',
+    component: CadastrarUsuario
+  },
+  {
+    path: 'cadastrar-cessionario',
+    component: CadastrarCessionario
   },
   {
     path: 'cadastrar-unidade',
--- frontend/src/app/pages/admin/admin.ts
+++ frontend/src/app/pages/admin/admin.ts
@@ -1,11 +1,83 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Navbar } from '../../components/navbar/navbar';
+
+type AdminMovimentacao = {
+  tipo: string;
+  movimentadoPor: string;
+  data: string;
+  hora: string;
+  cedidoA: string;
+};
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [Navbar, RouterLink],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
 export class Admin {
+
+  usuarios: AdminMovimentacao[] = [
+    {
+      tipo: 'Entrada',
+      movimentadoPor: 'Joao Pedro',
+      data: '12/03/2026',
+      hora: '14:43',
+      cedidoA: 'Pedro Henrique',
+    },
+    {
+      tipo: 'Entrada',
+      movimentadoPor: 'Joao Pedro',
+      data: '12/03/2026',
+      hora: '14:43',
+      cedidoA: 'Pedro Henrique',
+    },
+    {
+      tipo: 'Entrada',
+      movimentadoPor: 'Joao Pedro',
+      data: '12/03/2026',
+      hora: '14:43',
+      cedidoA: 'Pedro Henrique',
+    },
+    {
+      tipo: 'Entrada',
+      movimentadoPor: 'Joao Pedro',
+      data: '12/03/2026',
+      hora: '14:43',
+      cedidoA: 'Pedro Henrique',
+    },
+  ];
+
+  cessionarios: AdminMovimentacao[] = [
+    {
+      tipo: 'Entrada',
+      movimentadoPor: 'Joao Pedro',
+      data: '12/03/2026',
+      hora: '14:43',
+      cedidoA: 'Pedro Henrique',
+    },
+    {
+      tipo: 'Entrada',
+      movimentadoPor: 'Joao Pedro',
+      data: '12/03/2026',
+      hora: '14:43',
+      cedidoA: 'Pedro Henrique',
+    },
+    {
+      tipo: 'Entrada',
+      movimentadoPor: 'Joao Pedro',
+      data: '12/03/2026',
+      hora: '14:43',
+      cedidoA: 'Pedro Henrique',
+    },
+    {
+      tipo: 'Entrada',
+      movimentadoPor: 'Joao Pedro',
+      data: '12/03/2026',
+      hora: '14:43',
+      cedidoA: 'Pedro Henrique',
+    },
+  ];
 
 }
