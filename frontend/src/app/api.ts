44977516009471416279beee31a538734a24185f
Matheus <matheusaroxa13@gmail.com>
2026-06-06 18:25:09 -0300

    fix: corrigindo caminho do import do environment

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Api {
 
-  private baseUrl = "http://localhost:8080/api";
+  private baseUrl = `${environment.apiUrl}/api`;
 
   constructor(private http: HttpClient) {}
 
